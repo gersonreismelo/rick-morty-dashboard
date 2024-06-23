78321612dbcb24999f1ae562a9ad8291aeb2a7ca
Gerson dos Reis de Melo <gerson.jp.otaku@gmail.com>
2024-06-23 00:59:30 -0300

    feat: responsividade implementada

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 
@@ -8,7 +9,8 @@ import { FormsModule } from '@angular/forms';
   declarations: [],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    MatToolbarModule,
   ]
 })
 export class CoreModule { }
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -3,16 +3,19 @@ import { MatToolbarModule } from '@angular/material/toolbar'
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../features/service/auth.service';
 import { CommonModule } from '@angular/common';
+import { MatIconModule} from "@angular/material/icon"
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, RouterLink, CommonModule],
+  imports: [MatIconModule, RouterLink, CommonModule, MatToolbarModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   loggedInUser: string | null = null;
+
+  isMenuVisible = false;  
 
   constructor(private authService: AuthService, private router: Router) { }
 
@@ -25,6 +28,20 @@ export class HeaderComponent {
   logout(): void {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  async toggleMenu() {
+    this.isMenuVisible = !this.isMenuVisible; 
+    document.querySelector("html")!.style.overflow = this.isMenuVisible ? "hidden" : "auto";  
+
+    if (this.isMenuVisible) {
+      const mediaQuery = window.matchMedia("(max-width: 768px)");
+      mediaQuery.addEventListener("change", () => {
+        if (!mediaQuery.matches) {
+          this.toggleMenu(); 
+        }
+      });
+    }
   }
 
 }
